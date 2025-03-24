18ceeff9b9e0348f884a6fe03735fca23520d44b
0jc1 <0jc1@email.com>
2025-03-24 04:48:46 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# shellcheck disable=SC2059
+
+now=$(date +"%T")
+g='\033[0;32m'
+c='\033[0m'
+log_file="autovod_installation.log"
+
+# Log all the outputs of the script to the log file
+exec &> >(tee -a "$log_file")
+
+# Check if apt or dnf is installed
+if command -v apt-get &>/dev/null; then
+  package_manager='apt-get'
+  install_command='install'
+  update_command='update'
+  upgrade_command='upgrade'
+elif command -v dnf &>/dev/null; then
+  package_manager='dnf'
+  install_command='install'
+  update_command='check-update'
+  upgrade_command='upgrade'
+else
+  printf "${g}[$now] Error: Could not find a supported package manager. Exiting...${c}\n"
+  exit 1
+fi
+
+printf "${g}[$now] Updating and upgrading packages...${c}\n"
+$package_manager -qq $update_command && $package_manager -qq $upgrade_command
+
+printf "${g}[$now] Installing necessary Packages...${c}\n"
+
+# Package names for apt
+apt_packages=(npm wget curl git python3-pip tar jq)
+# Package names for DNF
+dnf_packages=(npm wget curl git python3-pip tar jq)
+
+# Use the appropriate package array based on the detected package manager
+if [ "$package_manager" = "apt-get" ]; then
+  packages=("${apt_packages[@]}")
+elif [ "$package_manager" = "dnf" ]; then
+  packages=("${dnf_packages[@]}")
+fi
+
+# Install the packages
+for package in "${packages[@]}"; do
+  dpkg -s "$package" &>/dev/null
+  if [ $? -eq 0 ]; then
+    printf "${g}[$now] $package already installed...${c}\n"
+  else
+    sudo $package_manager $install_command "$package" -y
+  fi
+done
+
+printf "${g}[$now] Installing PM2${c}\n"
+if ! [ -x "$(command -v pm2)" ]; then
+  npm install pm2 -g && pm2 startup
+else
+  printf "${g}[$now] PM2 is already installed. Skipping...${c}\n"
+fi
+
+printf "${g}[$now] Installing Streamlink${c}\n"
+if ! [ -x "$(command -v streamlink)" ]; then
+  pip3 install --upgrade streamlink
+else
+  printf "${g}[$now] Streamlink is already installed. Skipping...${c}\n"
+fi
+
+# ..
